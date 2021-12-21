7cc3c0501b358e6e71a2eb4aef2a90e8cb98fa7e
Lucas Adati de Paula <80161447+adaatii@users.noreply.github.com>
2021-12-21 17:25:44 -0300

    Funções e suas particularidades
    Exemplos realizados em aula sobre funções em JavaScript

---
+++ Sintaxe Básica em JavaScript/Aula 5 - Funções e suas particularidades/scripts.js
@@ -0,0 +1,11 @@
+/*function função() {
+    console.log("Essa mensagem veio de uma função");
+    
+}
+
+função();*/
+
+function menssagem(primeiro , segundo) {
+    console.log(primeiro , segundo)
+}
+menssagem('Tudo certo' , 'jovem !')
