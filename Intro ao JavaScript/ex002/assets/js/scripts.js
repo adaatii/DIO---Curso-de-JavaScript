8df6b0bf8618ea6f30986c50c2fff60827040a8b
Lucas Adati de Paula <80161447+adaatii@users.noreply.github.com>
2021-12-20 23:45:13 -0300

    Organizando pastas Curso JavaScript DIO

---
+++ Intro ao JavaScript/ex002/assets/js/scripts.js
@@ -0,0 +1,23 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+
+
+
+function increment(){
+    if(currentNumber<10){
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    
+}
+
+function decrement(){
+ 
+
+    if(currentNumber >-10 ){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    
+}
