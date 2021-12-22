c7ad8d82575aec698ba7a2df797da269d0c8d6eb
Lucas Adati de Paula <80161447+adaatii@users.noreply.github.com>
2021-12-21 22:34:49 -0300

    Aprofundando em funções
    O exercicio proposto foi a criação de uma calculadora utilizando JavaScript. O exercicio foi realizado em aula, onde foi empregado declaraçao das variaveis e estruturas condicionais como If/else e Switch Case, foi também paraticado e feito aprofundamento em funções

---
+++ Sintaxe Básica em JavaScript/Aula 6 -  Aprofundando em funções/scripts.js
@@ -0,0 +1,32 @@
+//Tipos de função
+
+//declarativas
+/*function funcao() {
+    console.log("Tudo certo!");
+    
+}
+
+funcao();*/
+
+//expressão de função
+//com nomeação
+/*var funcao = function funcao() {
+    console.log('Sou uma menssagem de função de expressão');   
+    
+}
+
+funcao();*/
+
+//sem nomecação
+/*var funcao = function () {
+    console.log('Sou uma menssagem de função de expressão');   
+    
+}
+
+funcao();*/
+
+/*var funcao = () =>{
+    console.log('Sou uma arrow function');
+}
+
+funcao();*/
