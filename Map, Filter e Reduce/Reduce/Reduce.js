503daff50c783f1b86d6302b7c3896358fa45361
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-10 15:29:05 -0300

    Atividade Reduce
    Foi criada uma função utilizando o .reduce para somar todos os mumeros de um array.

---
+++ Map, Filter e Reduce/Reduce/Reduce.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3];
+
+function somaNumeros(arr) {
+    return arr.reduce(function (prev, current) {
+        console.log({prev});
+        console.log({current});
+
+        return prev + current;
+    },0)
+    
+}
+
+console.log(somaNumeros(arr));
