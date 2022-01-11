ac5707efb8c85bf44ae2a8e5bac1dde86a437750
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-11 10:32:49 -0300

    Light Mode/Dark Mode

---
+++ Manipulando a D.O.M. com JavaScript/assets/js/scripts.js
@@ -0,0 +1,39 @@
+
+// Função
+function changeMode(){
+    changeClasses();
+    changeText();
+}
+
+function changeClasses(){
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText(){
+    const lightMode = 'Light Mode';
+    const darkMode = 'Dark Mode';
+    if (body.classList.contains(darkModeClass)) {
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + "ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + "ON";
+}
+
+
+const darkModeClass = 'dark-mode'
+
+//Seleção de elementos
+const button = document.getElementById('mode-selector') ;
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+// AddEventListener para que o evento seja escutado e retorne a função no console
+// Na ação do click chamar a função changeMode
+button.addEventListener('click', changeMode);
